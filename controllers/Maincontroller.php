8b0eaaa0549ab114bfe44066495ba18f9ff91d88
aoiryuusei8 <esdiaz.madariaga@gmail.com>
2026-04-28 21:54:13 -0400

    Add htmlspecialchars for input sanitization
    Sanitize input data for nuevoRegistro and nuevoContacto methods.

--- controllers/Maincontroller.php
+++ controllers/Maincontroller.php
@@ -4,16 +4,16 @@ require_once 'Models/Entidades.php';
 class MainController {
     public function nuevoRegistro($nombre, $correo) {
         $lector = new Lector();
-        $lector->nombre = $nombre;
-        $lector->correo = $correo;
+        $lector->nombre = htmlspecialchars($nombre);
+        $lector->correo = htmlspecialchars($correo);
         return $lector->registrarCuenta();
     }
 
     public function nuevoContacto($nombre, $email, $mensaje) {
         $contacto = new Contacto();
-        $contacto->nombre_remitente = $nombre;
-        $contacto->email_remitente = $email;
-        $contacto->mensaje = $mensaje;
+        $contacto->nombre_remitente = htmlspecialchars($nombre);
+        $contacto->email_remitente = htmlspecialchars($email);
+        $contacto->mensaje = htmlspecialchars($mensaje);
         return $contacto->enviarMensaje();
     }
 }
